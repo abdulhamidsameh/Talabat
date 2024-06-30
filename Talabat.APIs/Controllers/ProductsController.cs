0dcad52db8491d1e807e38899f1354ead7f61663
abdulhamidsameh <abdulhamid.sameh26@gmail.com>
2024-06-30 06:03:49 +0300

    Get Product By id End Point

--- Talabat.APIs/Controllers/ProductsController.cs
+++ Talabat.APIs/Controllers/ProductsController.cs
@@ -26,6 +26,17 @@ namespace Talabat.APIs.Controllers
 			return Ok(products);
 		}
 
+		// baseUrl/api/Products/id
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Product>>? GetProduct(int id)
+		{ 
+			var product = await _productRepo.GetAsync(id);
+			if(product is null)
+				return NotFound();
+
+			return Ok(product);
+
+		}
 
     }
 }
