28fe80296d5a8325178dcd715e4e5a2180cbfe6c
abdulhamidsameh <abdulhamid.sameh26@gmail.com>
2024-07-07 23:54:43 +0300

    Bugy Module

---
+++ Talabat.APIs/Controllers/BugyController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.Infrastructure.Data;
+
+namespace Talabat.APIs.Controllers
+{
+	public class BugyController : BaseApiController
+	{
+
+		// baseUrl/bugy/NotFound
+		[HttpGet("notfound")]
+		public ActionResult NotFoundRequest()
+		{
+			return NotFound();
+		}
+
+		// baseUrl/bugy/serverError
+		[HttpGet("servererror")]
+		public ActionResult ServerErrorRequest()
+		{
+			int X = 5;
+			int Y = 0;
+			int A = X /Y;
+			
+			return Ok(A);
+		}
+
+		// baseUrl/bugy/badrequest
+		[HttpGet("badrequest")]
+		public ActionResult GetBadRequest()
+		{
+			return BadRequest();
+		}
+
+		// baseUrl/bugy/badrequest/five
+		[HttpGet("badrequest/{id}")]
+		public ActionResult GetBadRequest(int id)
+		{
+			return Ok();
+		}
+    }
+}
