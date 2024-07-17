128f2db82cf91100b440c67c47b166a0d8b12c33
abdulhamidsameh <abdulhamid.sameh26@gmail.com>
2024-07-17 21:20:03 +0300

    Admin Dashboard and Mange Roles

---
+++ AdminDashboard/Controllers/RoleController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using AdminDashboard.Models;
+
+namespace AdminDashboard.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            // Get All Roles
+
+            var roles = await _roleManager.Roles.ToListAsync();
+
+
+            return View(roles);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(RoleFormViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var role = await _roleManager.RoleExistsAsync(model.Name);
+                if (!role)
+                {
+                    await _roleManager.CreateAsync(new IdentityRole(model.Name));
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ModelState.AddModelError("Name", "Role Is Exist");
+                    return View("Index", await _roleManager.Roles.ToListAsync());
+                }
+            }
+            return RedirectToAction(nameof(Index));
+
+        }
+        public async Task<IActionResult> Delete(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role is not null)
+                await _roleManager.DeleteAsync(role);
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            var mapRole = new RoleViewModel()
+            {
+                Name = role!.Name!,
+            };
+            return View(mapRole);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, RoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var roleExist = await _roleManager.RoleExistsAsync(id);
+                if (!roleExist)
+                {
+                    var role = await _roleManager.FindByIdAsync(model.Id);
+                    role!.Name = model.Name;
+                    await _roleManager.UpdateAsync(role);
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ModelState.AddModelError("Name", "Role Is Exist");
+                    return View("Index", await _roleManager.Roles.ToListAsync());
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
